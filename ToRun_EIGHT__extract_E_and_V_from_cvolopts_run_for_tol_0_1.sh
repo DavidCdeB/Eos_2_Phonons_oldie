e48c824c03e91ae8641d09ca3a656ad3c614a1ef
David Carrasco de Busturia <davidcarrascobustur@gmail.com>
2017-10-12 18:29:33 +0100

    Add files via upload

--- ToRun_EIGHT__extract_E_and_V_from_cvolopts_run_for_tol_0_1.sh
+++ ToRun_EIGHT__extract_E_and_V_from_cvolopts_run_for_tol_0_1.sh
@@ -9,6 +9,6 @@ bash EIGHT__extract_E_and_V_from_cvolopts_run_for_tol_0_1.sh > E_vs_V_Calcite_II
 
 sed -i '1i\#Volume    Energy' E_vs_V_Calcite_II.dat
 
-
+sort -k1 -n E_vs_V_Calcite_II.dat -o E_vs_V_Calcite_II.dat
 
 
